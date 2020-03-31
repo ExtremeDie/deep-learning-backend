076997208ec989b8875d90cde01c2d4ea7c4a951
YongZhuang Koay <yongzhuang@wtech.software>
2020-03-31 16:43:12 +0800

    changed to port 80

--- deep_learning_backend/main.py
+++ deep_learning_backend/main.py
@@ -20,4 +20,4 @@ class status(Resource):
 
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', debug=True) # port 5000
-    app.run(host='0.0.0.0') # port 5000
+    app.run(host='0.0.0.0', port=80) # port 5000
